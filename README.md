51ed614d203831e62bc6c25beb3d8367bf90d23f
Sam <villaluzsam@gmail.com>
2017-10-07 00:23:27 -0400

    add more review metadata

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# Data Mining project using music reviews from https://pitchfork.com/
+# Data Mining Project (MSCI 446)
+
+Using music reviews to predict album ratings, scrobbles and listens (using https://pitchfork.com/reviews/albums/ and https://last.fm)
+
+##Team Members
+
+Laura Arrizza, Caroline Davey, Sam Villaluz, Liam Yeates
--- scrape.py
+++ scrape.py
@@ -2,15 +2,69 @@ import urllib
 import json
 import requests
 from bs4 import BeautifulSoup
+import csv
 
 pageNum = 1
 pitchforkUrl = "https://pitchfork.com/"
+my_list = []
 
+def artist(soup):
+    name = soup.find("h2",{"class":"artists"}).string.encode('utf-8')
+    return(name)
+
+def rate(soup):
+    rating = soup.find("span", {"class":"score"}).string
+    return float(rating)
+
+def albumName(soup):
+    name = soup.find("h1",{"class":"review-title"}).string.encode('utf-8')
+    return(name)
+
+def bnm(soup): # boolean to determine if it has a best new music tag
+    bnm = soup.find("p",{"class":"bnm-txt"})
+    if bnm is None:
+        return 0
+    else:
+        return 1
+
+def genre(soup):
+    for ul in soup.find_all('ul', class_='genre-list'):
+        type = ul.a.string.encode('utf-8') 
+        return(type)
+
+def pubDate(soup):
+    date = soup.find('time', {'class':'pub-date'}).string.encode('utf-8')
+    return(date)
+
+def abstract(soup): # short summary of album before actual review
+    desc = soup.find('div', {'class':'abstract'}).text.encode('utf-8').rstrip()
+    return(desc)
+
+#to be cont: pull whole review
+    
 def initializeSoup(url):
     request = requests.get(url)
     soup = BeautifulSoup(request.text, 'html.parser')
     return soup
 
+def review(url): #gets all metadata from each album review and puts it into a list
+    fullUrl = "https://pitchfork.com/" + url
+    soup = initializeSoup(fullUrl)
+    
+    lst = []
+    lst.append(artist(soup))
+    lst.append(rate(soup))
+    lst.append(albumName(soup))
+    lst.append(bnm(soup))
+    lst.append(genre(soup))
+    lst.append(pubDate(soup))
+    lst.append(abstract(soup))
+    print(lst)
+    
+    my_list.append(lst)
+
+
+# iterates through every page of 'Reviews' and finds URLs
 while (pageNum < 2):
     website = "https://pitchfork.com/reviews/albums/?page=" + str(pageNum)
     soup = initializeSoup(website)
@@ -21,7 +75,12 @@ while (pageNum < 2):
     for albumLink in albumLinks:
         link = albumLink.find("a", {"class":"album-link"})
         url = link.get("href")
-        print(url)
-
+        review(url)
+        
     pageNum+=1
+
+# write to csv
+with open("output.csv", "wb") as f:
+    writer = csv.writer(f)
+    writer.writerows(my_list)
         
